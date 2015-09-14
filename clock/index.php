fcf41270d4f913ac30851fc76d9705763d16a061
Mikko Heikkinen <hawk@biomi.org>
2015-09-14 22:27:13 +0300

    File reorganization

---
+++ clock/index.php
@@ -0,0 +1,53 @@
+<div id="d-clock" class="widget">
+
+    <style type="text/css">
+       
+        #clock {
+
+        	width: 600px;
+        	height: 600px;
+        	background: url(clock/images/Clock3wo-border-black.png);
+        	list-style: none;
+				-webkit-transform: scale(0.5,0.5); /* Safari */
+				transform: scale(0.5,0.5);
+
+        	}
+        
+        #sec, #min, #hour {
+        	position: absolute;
+        	width: 30px;
+        	height: 600px;
+        	top: 0px;
+        	left: 285px;
+        	}
+        
+        #sec {
+        	background: url(clock/images/sechand.png);
+        	z-index: 1;
+        	opacity: 0.35;
+           	}
+        #sec img
+        {
+			
+        }
+           
+        #min {
+        	background: url(clock/images/minhand.png);
+        	z-index: 4;
+           	}
+           
+        #hour {
+        	background: url(clock/images/hourhand.png);
+        	z-index: 2;
+           	}
+           	
+    </style>
+
+    <script src="vendor/CSS3Clock/script.js" type="text/javascript"></script>
+
+	<ul id="clock">	
+	   	<li id="sec"></li>
+	   	<li id="hour"></li>
+		<li id="min"></li>
+	</ul>
+</div>
--- index.php
+++ index.php
@@ -14,55 +14,26 @@
         	padding: 0;
         	background-color: #000;
         }
-        
-        #clock {
-        	position: absolute;
-        		top: -150px;
-        		left: -150px;
-        	width: 600px;
-        	height: 600px;
-        	background: url(images/clock/Clock3wo-border-black.png);
-        	list-style: none;
-				-webkit-transform: scale(0.5,0.5); /* Safari */
-				transform: scale(0.5,0.5);
-
-        	}
-        
-        #sec, #min, #hour {
-        	position: absolute;
-        	width: 30px;
-        	height: 600px;
-        	top: 0px;
-        	left: 285px;
-        	}
-        
-        #sec {
-        	background: url(images/clock/sechand.png);
-        	z-index: 1;
-        	opacity: 0.35;
-           	}
-        #sec img
-        {
-			
-        }
-           
-        #min {
-        	background: url(images/clock/minhand.png);
-        	z-index: 4;
-           	}
-           
-        #hour {
-        	background: url(images/clock/hourhand.png);
-        	z-index: 2;
-           	}
            	
         p {
             text-align: center; 
             padding: 10px 0 0 0;
             }
+
+        .widget
+        {
+            border: 1px solid cyan;
+        }
+
+        
+        #d-clock
+        {
+            position: absolute;
+            top: -150px; /* positioned this way because scaling leaves empty margins */
+            left: -150px;
+        }
     </style>
     
-    <script src="vendor/CSS3Clock/script.js" type="text/javascript"></script>
     <!--
     https://css-tricks.com/css3-clock/
 	http://freedesignfile.com/40901-different-clock-design-vector-03/
@@ -73,11 +44,7 @@
 
 <body>
 
-	<ul id="clock">	
-	   	<li id="sec"></li>
-	   	<li id="hour"></li>
-		<li id="min"></li>
-	</ul>
+<?php include_once "clock/index.php"; ?>
 	
 </body>
 	
