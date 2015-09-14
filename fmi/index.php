43f77df52196f636d665bae01c6b956f2a03d79c
Mikko Heikkinen <hawk@biomi.org>
2015-09-14 23:08:31 +0300

    Read FMI open data from XML API (init)

---
+++ fmi/index.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+include_once "../../api-key.php";
+
+//echo $apiKey; // debug
+
+$url = "http://data.fmi.fi/fmi-apikey/" . $apiKey . "/wfs?request=getFeature&storedquery_id=fmi::observations::weather::mast::multipointcoverage&fmisid=101000&timestep=10&";
+
+$xmlString = file_get_contents($url);
+
+$xml = simplexml_load_string($xmlString);
+
+print_r ($xml); // debug
+
+?>
