5d6d66215504f5decd16a53401ed71707281a513
Mikko Heikkinen <hawk@biomi.org>
2015-09-15 01:01:27 +0300

    Simple UI for mast temps

--- fmi/index.php
+++ fmi/index.php
@@ -2,4 +2,33 @@
 
 include "mast.php";
 
+
+
 ?>
+
+<style>
+#box
+{
+	border: 1px solid blue;
+	position: relative;
+	height: 400px;
+}
+#box div
+{
+	border: 1px solid lime;
+	position: absolute;
+	left: 0px;
+
+}
+</style>
+
+<div id="box">
+	<?php
+
+	foreach ($tuples as $h => $t)
+	{
+		echo "<div style=\"bottom: " . $h . "px; width: " . $t*5 . "px;\">$t</div>";
+	}
+
+	?>
+</div>
