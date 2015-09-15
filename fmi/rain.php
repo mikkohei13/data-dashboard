01028950a4eb74201a1b9f2032946b04fe00ee3c
Mikko Heikkinen <hawk@biomi.org>
2015-09-15 22:53:53 +0300

    Go box through pixel by pixel

--- fmi/rain.php
+++ fmi/rain.php
@@ -12,6 +12,15 @@ imageline ( $im , 385 , 150 , 385 , 349 , $color );
 imageline ( $im , 385 , 349 , 186 , 349 , $color );
 imageline ( $im , 186 , 349 , 186 , 150 , $color );
 
+for ($y=150; $y <= 349; $y++)
+{ 
+	for ($x=186; $x <= 385; $x++)
+	{ 
+		imagesetpixel ( $im , $x , $y , $color );
+	}
+}
+
+
 if ($im)
 {
   header("Content-type: image/png");
