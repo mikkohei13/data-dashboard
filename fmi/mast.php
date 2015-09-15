6fdce6c69a2867870ac734e1a7e929f54cf1e08b
Mikko Heikkinen <hawk@biomi.org>
2015-09-15 21:29:15 +0300

    Humidity measurements

--- fmi/mast.php
+++ fmi/mast.php
@@ -14,25 +14,30 @@ $xmlString = str_replace(":", "_", $xmlStringWithNamespaces);
 
 $xml = simplexml_load_string($xmlString);
 
+$datetime = FALSE;
+
 foreach($xml->wfs_member as $member)
 {
 	// Time
-	$timeRaw = $member->omso_ProfileObservation->om_resultTime->gml_TimeInstant->gml_timePosition;
-	$timeRaw = str_replace("_", ":", $timeRaw); // includes timezone
+	if ($datetime === FALSE)
+	{
+		$timeRaw = $member->omso_ProfileObservation->om_resultTime->gml_TimeInstant->gml_timePosition;
+		$timeRaw = str_replace("_", ":", $timeRaw); // includes timezone
 
-	date_default_timezone_set('Europe/Helsinki');
-	$datetime = new DateTime($timeRaw);
-	$helsinkiTime = new DateTimeZone('Europe/Helsinki');
-	$datetime->setTimezone($helsinkiTime);
-//	echo $datetime->format('Y-m-d H.i.s'); // debug
+		date_default_timezone_set('Europe/Helsinki');
+		$datetime = new DateTime($timeRaw);
+		$helsinkiTime = new DateTimeZone('Europe/Helsinki');
+		$datetime->setTimezone($helsinkiTime);
+	//	echo $datetime->format('Y-m-d H.i.s'); // debug
+	}
 
 	// Measurements
 	$multiPointCoverage = $member->omso_ProfileObservation->om_result->gmlcov_MultiPointCoverage;
 	$att = $multiPointCoverage->attributes();
 
-	if ($att['gml_id'] == "mpcv1-1") // temperature
+	// Temperature and height
+	if ($att['gml_id'] == "mpcv1-1")
 	{
-		echo "Getting temp";
 		$heightsString = $multiPointCoverage->gml_domainSet->gmlcov_SimpleMultiPoint->gmlcov_positions;
 		$heightsString = trim($heightsString);
 		$heights = explode(" ", $heightsString);
@@ -44,9 +49,28 @@ foreach($xml->wfs_member as $member)
 		foreach ($heights as $nro => $height)
 		{
 			echo "x";
-			$tuples[$height] = $measurements[$nro];
+			$heightTuples[$height] = $measurements[$nro];
 		}
 	}
+	// Temperature and height
+	elseif ($att['gml_id'] == "mpcv1-2")
+	{
+		$heightsString = $multiPointCoverage->gml_domainSet->gmlcov_SimpleMultiPoint->gmlcov_positions;
+		$heightsString = trim($heightsString);
+		$heights = explode(" ", $heightsString);
+
+		$measurementsStrings = $multiPointCoverage->gml_rangeSet->gml_DataBlock->gml_doubleOrNilReasonTupleList;
+		$measurementsStrings = trim($measurementsStrings);
+		$measurements = explode(" ", $measurementsStrings);
+
+		foreach ($heights as $nro => $height)
+		{
+			echo "x";
+			$humidityTuples[$height] = $measurements[$nro];
+		}
+	}
+
+
 
 
 }
@@ -55,7 +79,8 @@ foreach($xml->wfs_member as $member)
 echo $datetime->format('Y-m-d H.i.s');
 print_r ($heights);
 print_r ($measurements);
-print_r ($tuples);
+print_r ($heightTuples);
+print_r ($humidityTuples);
 
 
 
