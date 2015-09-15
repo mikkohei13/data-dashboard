d4120bacc889e2183b4f17257a6b0f181ad4dbe7
Mikko Heikkinen <hawk@biomi.org>
2015-09-15 22:48:20 +0300

    Testing image dimensions

--- fmi/rain.php
+++ fmi/rain.php
@@ -2,6 +2,10 @@
 
 $im = imagecreatefrompng("testbed-testdata.png");
 
+$color = imagecolorallocate ( $im , 0xFF , 0x00 , 0x00 );
+
+imageline ( $im , 186 , 150 , 385 , 349 , $color ); // 200*200 px box, centered on Kauklahti
+
 if ($im)
 {
   header("Content-type: image/png");
