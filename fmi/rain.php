e760e141882afb57a3edfb99a29247de2495c4c3
Mikko Heikkinen <hawk@biomi.org>
2015-09-16 00:10:15 +0300

    Debug option to show image

--- fmi/rain.php
+++ fmi/rain.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+Debug:
+http://localhost/data-dashboard/fmi/rain.php?debug
+
+Normal:
+http://localhost/data-dashboard/fmi/rain.php
+
+*/
+
 $indexValues['250-120-255'] = 50;
 $indexValues['255-80-60'] = 40;
 $indexValues['255-150-50'] = 34;
@@ -11,7 +20,23 @@ $indexValues['10-155-225'] = 4;
 
 $masterIndex = 0;
 
-$im = imagecreatefrompng("testbed-testdata.png");
+$divider = 100;
+
+// ----------------------
+
+// TODO: more change-tolerant way of getting the file?
+$testbedHTML = file_get_contents("http://testbed.fmi.fi/");
+
+$start = strpos($testbedHTML, '<img src="data/area/radar/temperature/');
+$end = strpos($testbedHTML, '.png" width=600 height=508 border=0');
+
+$len = $end - $start;
+
+$imageUrl = "http://testbed.fmi.fi/" . substr($testbedHTML, $start+10, $len-6);
+//echo $imageUrl;
+
+$im = imagecreatefrompng($imageUrl);
+//$im = imagecreatefrompng("testbed-testdata.png"); // debug
 
 $color = imagecolorallocate ( $im , 0xFF , 0x00 , 0x00 );
 
@@ -19,20 +44,21 @@ $color = imagecolorallocate ( $im , 0xFF , 0x00 , 0x00 );
 // DEBUG
 //imageline ( $im , 186 , 150 , 385 , 349 , $color ); // 200*200 px diagonal line, centered on Kauklahti
 
-// 200*200 px box, centered on Kauklahti
 
-/*
-imageline ( $im , 186 , 150 , 385 , 150 , $color );
-imageline ( $im , 385 , 150 , 385 , 349 , $color );
-imageline ( $im , 385 , 349 , 186 , 349 , $color );
-imageline ( $im , 186 , 349 , 186 , 150 , $color );
-
-if ($im)
+if (isset($_GET['debug']))
 {
-  header("Content-type: image/png");
-  imagepng($im);
+	// 200*200 px box, centered on Kauklahti
+	imageline ( $im , 186 , 150 , 385 , 150 , $color );
+	imageline ( $im , 385 , 150 , 385 , 349 , $color );
+	imageline ( $im , 385 , 349 , 186 , 349 , $color );
+	imageline ( $im , 186 , 349 , 186 , 150 , $color );
+
+	if ($im)
+	{
+	  header("Content-type: image/png");
+	  imagepng($im);
+	}
 }
-*/
 
 
 for ($y=150; $y <= 349; $y++)
@@ -47,10 +73,10 @@ for ($y=150; $y <= 349; $y++)
 
 		imagesetpixel ( $im , $x , $y , $color );
 	}
-//	echo "row $y / masterIndex: " . ($masterIndex / 1000) . "<br />\n";
+//	echo "row $y / masterIndex: " . ($masterIndex / $divider) . "<br />\n";
 }
 
-echo "Rain index: " . round(($masterIndex / 1000), 0) . "<br />\n";
+echo "Rain index: " . round(($masterIndex / $divider), 0) . "<br />\n";
 
 
 function add2index($rgb)
