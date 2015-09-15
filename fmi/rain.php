0d4e75a665b6f6d6b717a15f619f3ac6fa466c0b
Mikko Heikkinen <hawk@biomi.org>
2015-09-15 22:51:15 +0300

    200px bounding box around Kauklahti

--- fmi/rain.php
+++ fmi/rain.php
@@ -4,7 +4,13 @@ $im = imagecreatefrompng("testbed-testdata.png");
 
 $color = imagecolorallocate ( $im , 0xFF , 0x00 , 0x00 );
 
-imageline ( $im , 186 , 150 , 385 , 349 , $color ); // 200*200 px box, centered on Kauklahti
+//imageline ( $im , 186 , 150 , 385 , 349 , $color ); // 200*200 px diagonal line, centered on Kauklahti
+
+// 200*200 px box, centered on Kauklahti
+imageline ( $im , 186 , 150 , 385 , 150 , $color );
+imageline ( $im , 385 , 150 , 385 , 349 , $color );
+imageline ( $im , 385 , 349 , 186 , 349 , $color );
+imageline ( $im , 186 , 349 , 186 , 150 , $color );
 
 if ($im)
 {
