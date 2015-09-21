35cd383a4ac6647dd0466120897c983b9e971e6b
Mikko Heikkinen <hawk@biomi.org>
2015-09-21 11:12:41 +0300

    List of buses

--- bus/index.php
+++ bus/index.php
@@ -7,15 +7,30 @@ $url = "http://api.reittiopas.fi/hsl/prod/?user=" . $HSLtoken . "&pass=" . $HSLp
 $json = file_get_contents($url);
 $arr = json_decode($json, TRUE);
 
+$limit = 4;
+
 //print_r ($arr); exit(); // debug 
+
+echo "<div>";
+echo "<span class=\"label\">Buses to W:</span>";
 
 foreach ($arr[0]['departures'] as $number => $busArr)
 {
-	print_r($busArr);
+//	print_r($busArr);
+
 //	$line = explode(" ", $busArr['code']);
 	$line = substr($busArr['code'], 1, 3);
 	$line = ltrim($line, 0);
-	echo "LINE:" . $line . "\n";
+
+	$time = substr($busArr['time'], 0, 2) . "." . substr($busArr['time'], 2, 2);
+
+	echo "&nbsp;<span class=\"value\">" . $line . " " . $time . "</span>\n";
+
+	$i++;
+	if ($i >= $limit)
+	{
+		break;
+	}
 }
 
-
+echo "</div>";
