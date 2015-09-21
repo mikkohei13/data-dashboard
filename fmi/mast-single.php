4bf52e7035702d71c8ecd4c25c6453d175f407cf
Mikko Heikkinen <hawk@biomi.org>
2015-09-21 13:48:46 +0300

    Wind direction bugfix

--- fmi/mast-single.php
+++ fmi/mast-single.php
@@ -33,7 +33,7 @@ include_once "mast-fetch.php";
 	foreach ($measurements['WD'] as $h => $t)
 	{
 		$t = $t + 180;
-		echo "<strong><span class=\"value\" style=\"-webkit-transform: rotate(" . $t . "deg);\">&#8593;</span></strong> ";
+		echo "<strong><span class=\"value\" style=\"-webkit-transform: rotate(" . $t . "deg); display: inline-block;\">&#8593;</span></strong> ";
 		break;
 	}
 
