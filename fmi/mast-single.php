7a74b03393a249c56839ad61175852398b4bf188
Mikko Heikkinen <hawk@biomi.org>
2015-09-23 08:56:15 +0300

    Handle missing mast data

--- fmi/mast-single.php
+++ fmi/mast-single.php
@@ -12,37 +12,45 @@ include_once "mast-fetch.php";
 	<span class="label">Latest</span> 
 	<?php
 
-	$scale = 1.2;
+	if (! empty($measurements))
+	{
+		$scale = 1.2;
 
-	foreach ($measurements['TA'] as $h => $t)
-	{
-		echo "<span class=\"value\">$t</span><span class=\"unit\">&deg;C</span> ";
-		break;
-	}
-	foreach ($measurements['RH'] as $h => $t)
-	{
-		$t = round($t, 0);
-		echo "&nbsp;<span class=\"value\">$t</span><span class=\"unit\">%</span> ";
-		break;
-	}
-	foreach ($measurements['WS'] as $h => $t)
-	{
-		echo "&nbsp;<span class=\"value\">$t</span> <span class=\"unit\">m/s</span> ";
-		break;
-	}
-	foreach ($measurements['WD'] as $h => $t)
-	{
-		$t = $t + 180;
-		echo "<strong><span class=\"value\" style=\"-webkit-transform: rotate(" . $t . "deg); display: inline-block;\">&#8593;</span></strong> ";
-		break;
-	}
+		foreach ($measurements['TA'] as $h => $t)
+		{
+			echo "<span class=\"value\">$t</span><span class=\"unit\">&deg;C</span> ";
+			break;
+		}
+		foreach ($measurements['RH'] as $h => $t)
+		{
+			$t = round($t, 0);
+			echo "&nbsp;<span class=\"value\">$t</span><span class=\"unit\">%</span> ";
+			break;
+		}
+		foreach ($measurements['WS'] as $h => $t)
+		{
+			echo "&nbsp;<span class=\"value\">$t</span> <span class=\"unit\">m/s</span> ";
+			break;
+		}
+		foreach ($measurements['WD'] as $h => $t)
+		{
+			$t = $t + 180;
+			echo "<strong><span class=\"value\" style=\"-webkit-transform: rotate(" . $t . "deg); display: inline-block;\">&#8593;</span></strong> ";
+			break;
+		}
 
-/*
-	foreach ($measurements['TA'] as $h => $t)
-	{
-		echo "<div style=\"bottom: " . $h . "px; width: " . $t*5 . "px;\">$t</div>";
+	/*
+		foreach ($measurements['TA'] as $h => $t)
+		{
+			echo "<div style=\"bottom: " . $h . "px; width: " . $t*5 . "px;\">$t</div>";
+		}
+	*/
 	}
-*/
+	else
+	{
+		echo "<!-- Mastohavaintoja ei saatavilla -->";
+		echo "</div>";
+	}
 
 	?>
 </div>
