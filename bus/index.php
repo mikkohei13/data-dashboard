7c5a6a7b1d93cbc0034c4785aeb464e114097d79
Mikko Heikkinen <hawk@biomi.org>
2015-10-11 00:32:54 +0300

    Handle missing buses

--- bus/index.php
+++ bus/index.php
@@ -2,18 +2,25 @@
 
 require_once "../../api-key-hsl.php";
 
-$url = "http://api.reittiopas.fi/hsl/prod/?user=" . $HSLtoken . "&pass=" . $HSLpass . "&request=stop&code=2511204"; // Erik Basse, kohti Helsinkiä
+$limit = 4;
+
+$url = "http://api.reittiopas.fi/hsl/prod/?user=" . $HSLtoken . "&pass=" . $HSLpass . "&request=stop&code=2511204&time_limit=360&dep_limit=" . $limit; // Erik Basse, kohti Helsinkiä
 
 $json = file_get_contents($url);
 $arr = json_decode($json, TRUE);
 
-$limit = 4;
 
 //print_r ($arr); exit(); // debug 
 
 echo "<span class=\"label\">Buses</span>";
 
 $i = 0;
+
+if (! is_array($arr[0]['departures']))
+{
+	exit("<!-- no buses -->");
+}
+
 foreach ($arr[0]['departures'] as $number => $busArr)
 {
 //	print_r($busArr);
@@ -38,9 +45,5 @@ foreach ($arr[0]['departures'] as $number => $busArr)
 	echo "&nbsp;<span class=\"value\"><strong>" . $line . "</strong> " . $time . "</span>\n";
 
 	$i++;
-	if ($i >= $limit)
-	{
-		break;
-	}
 }
 
