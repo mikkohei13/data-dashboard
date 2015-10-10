fd39d9442b5d4c1602238575a272ae69ee3cf5e2
Mikko Heikkinen <hawk@biomi.org>
2015-10-10 21:11:03 +0300

    Day length with suncalc

--- sun/suncalc.php
+++ sun/suncalc.php
@@ -1,11 +1,31 @@
 
 <span class="label">Sunrise</span> <span class="value" id="sunrise"></span>
 &nbsp;<span class="label">set</span> <span class="value" id="sunset"></span>
-&nbsp;<span class="label">day length</span> <span class="value"></span>
+&nbsp;<span class="label">day length</span> <span class="value" id="daylength"></span>
 
 <script>
 
 <?php include 'vendor/suncalc-master/suncalc-wo-function.js'; ?>
+
+function dhm(t){
+    var cd = 24 * 60 * 60 * 1000,
+        ch = 60 * 60 * 1000,
+        d = Math.floor(t / cd),
+        h = Math.floor( (t - d * cd) / ch),
+        m = Math.round( (t - d * cd - h * ch) / 60000),
+        pad = function(n){ return n < 10 ? '0' + n : n; };
+  if( m === 60 ){
+    h++;
+    m = 0;
+  }
+  if( h === 24 ){
+    d++;
+    h = 0;
+  }
+  return pad(h) +' h ' + pad(m) + ' min';
+}
+
+
 
 var lat = 60.19;
 var lng = 24.59;
@@ -16,11 +36,15 @@ var times = SunCalc.getTimes(new Date(), lat, lng);
 var sunriseStr = times.sunrise.getHours() + '.' + times.sunrise.getMinutes();
 var sunsetStr = times.sunset.getHours() + '.' + times.sunset.getMinutes();
 
+var dayLengthMillisec =  times.sunset.getTime() - times.sunrise.getTime();
+var dayLengthStr = dhm(dayLengthMillisec);
+
 </script>
 <script>
 
 $( "#sunrise" ).text( sunriseStr );
 $( "#sunset" ).text( sunsetStr );
+$( "#daylength" ).text( dayLengthStr );
 
 </script>
 
