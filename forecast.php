f0e1e5741985636285496d40f1a88c2e97a6fbdf
Mikko Heikkinen <hawk@biomi.org>
2015-10-17 16:37:05 +0300

    Forecast as iframe

---
+++ forecast.php
@@ -0,0 +1,10 @@
+<style>
+iframe
+{
+	width: 100%;
+	height: 100%;
+	border: 0;
+	margin: 0;
+}
+</style>
+<iframe src="http://ilmatieteenlaitos.fi/saa/espoo/kauklahti#p_p_id_localweatherportlet_WAR_fmiwwwweatherportlets_"></iframe>
