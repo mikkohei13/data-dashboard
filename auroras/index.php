346175a59252d0602adb40392144679dda337383
Mikko Heikkinen <hawk@biomi.org>
2015-10-18 11:06:14 +0300

    Show image time in minutes

--- auroras/index.php
+++ auroras/index.php
@@ -1,6 +1,7 @@
 <?php
+date_default_timezone_set("Europe/Helsinki");
 
-if ($_GET['loc'] == "HOV")
+if (@$_GET['loc'] == "HOV")
 {
 	$imageUrl ="http://aurora.fmi.fi/public_service/pienempi_HOV.jpg";
 }
@@ -9,7 +10,26 @@ else
 	$imageUrl ="http://aurora.fmi.fi/public_service/latest_pieni_DYN.jpg";
 }
 
+//echo $imageUrl;
+$exif = exif_read_data($imageUrl);
+
+$imageTimestamp = strtotime($exif['DateTime']);
+$imageAgeSeconds = time() - $imageTimestamp - (3*60*60); // substract clock error
+$imageAgeMinutes = round(($imageAgeSeconds / 60), 0);
+
+//echo "image age: " . $imageAgeSeconds;
+
+//echo "\n" . gmdate("Y-m-d H:i:s", $imageTimestamp) . "\n"; // debug
+
+//print_r ($exif);
+
+
+//exit("END");
+
 $im = imagecreatefromjpeg($imageUrl);
+
+$textcolor = imagecolorallocate($im, 255, 255, 255);
+imagestring ( $im , 10 , 5 , 5 , ($imageAgeMinutes . " min") , $textcolor );
 
 if ($im)
 {
